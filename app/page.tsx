17f698d34cb5b542341d8722b21d7fcb071adb51
Gabriel Gomes <gomesg827@gmail.com>
2024-11-16 12:01:02 -0300

    Configuração geral da página e criando a Página Inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Inicio from "@/components/Inicio";
+
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <h1 className="max-w-7xl w-full">
+        <Inicio />
+      </h1>
+    </div>
   );
 }
