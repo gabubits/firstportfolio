834d14faec9ddf0cad06e1058dda323de73ec031
Gabriel Gomes <gomesg827@gmail.com>
2024-11-20 11:23:22 -0300

    Telas do projeto base concluídas

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Inicio from "@/components/Inicio";
 import Projetos from "@/components/Projetos";
 import { FloatingNav } from "@/components/ui/FloatingNavBar";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <h1 className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Inicio />
         <Grid />
         <Projetos />
+        <Clients />
+        <Experience />
+        <Footer />
       </h1>
     </div>
   );
